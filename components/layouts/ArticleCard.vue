13c2f4e8c658463b20b55b81f2b6bbd4d4fa7c3e
danimpulse500 <danimpulse500@gmail.com>
2026-04-03 16:08:57 +0100

    reduced article size

--- components/layouts/ArticleCard.vue
+++ components/layouts/ArticleCard.vue
@@ -4,7 +4,7 @@
       <img class="lg:h-[306px] md:h-36 w-full object-cover object-center" :src="article?.image_url || placeholderImg" alt="blog">
       <div class="mt-4">
       
-        <h1 class="title-font text-2xl font-semibold text-[#101828] mb-3">{{ article?.title }}</h1>
+        <h3 class="text-base font-bold text-gray-900 mb-2 group-hover:text-primary transition-colors line-clamp-2 uppercase">{{ article?.title }}</h3>
         <p class="leading-relaxed mb-3 text-secondary-body-regular-contrast line-clamp-3" v-html="article?.content"></p>
         <div class="flex items-center flex-wrap ">
           <NuxtLink :to="`/general/article/${article?.id}`" class="text-primary inline-flex items-center font-medium md:mb-2 lg:mb-0">
